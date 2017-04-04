d3602b433dd9ddabfe951ed5e6f2cb5343d42018
Syed Khurshid <magnite@SyedKhushidsMBP.lan>
2017-04-03 21:46:44 -0500

    Added the server.js and working to function the app"

---
+++ server.js
@@ -0,0 +1,17 @@
+// dependencies//
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// Sets an initial port. We"ll use this later in our listener//
+var PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
