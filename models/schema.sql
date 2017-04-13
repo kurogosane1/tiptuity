e43449a3561744a81f2c3cd3410739820bc29dcc
Syed Khurshid <magnite@SyedKhushidsMBP.lan>
2017-04-13 00:13:53 -0500

    added the model folder and SQL

---
+++ models/schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS tips_db;
+
+-- we are telling the system to use burger_db
+USE burger_db;
+
+CREATE TABLE IF NOT EXISTS employees
+(
+    employee_id INT(30)AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(60),
+    employee_address VARCHAR(90),
+    date_joined DATE
+);
+
+-- this the tips table to show the collected amount--
+CREATE TABLE IF NOT EXISTS tips 
+(
+	tip_id INT(30)AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	employee_id SMALLINT UNSIGNED NOT NULL REFERENCES `employees`(`employee_id`),
+	tip_amount INT(30) NOT NULL,
+	tip_fees INT(30) NOT NULL,
+	tip_final INT(30) NOT NULL	
+);
+
+
+
---
+++ models/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees VALUES ("Syed", "Khurshid", "ssk@gmail.com", "some address", 09-19-2016);
+INSERT INTO employees VALUES ("Alex", "Slayton", "alex@gmail.com", "some address", 01-20-2016);
+INSERT INTO employees VALUES ("Faisal", "Nasir", "faisal@gmail.com", "some address", 06-01-2016);
+INSERT INTO tips VALUES ("Syed", "Khurshid", "ssk@gmail.com", "some address", 09-19-2016);
+
+
