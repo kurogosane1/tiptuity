020caa24808a424f97d84093b1d7d3128ba3452a
Syed Khurshid <magnite@Syed-Khurshids-MacBook-Pro.local>
2017-04-21 21:53:42 -0500

    added a node version in package.json file

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO employees VALUES (2,"Syed", "Khurshid", "ssk@gmail.com", "some address", "2016-09-19");
 INSERT INTO employees VALUES (1, "Alex", "Slayton", "alex@gmail.com", "some address", "2016-02-20");
-INSERT INTO employees VALUES (3,"Faisal", "Nasir", "faisal@gmail.com", "some address", "2016-06-01");
-INSERT INTO tips VALUES (1, 0.33, 0.67) WHERE employee_id = 2;
+INSERT INTO employees VALUES (3,"Faisal", "Narsi", "faisal@gmail.com", "some address", "2016-06-01");
+INSERT INTO tips VALUES (1.00, 0.33, 0.67) WHERE employee_id = 2;
 INSERT INTO tips VALUES (20, 0.33, 19.67) WHERE employee_id = 1;
 INSERT INTO tips VALUES (20, 0.33, 19.67) WHERE employee_id = 2;
 INSERT INTO tips VALUES (20, 0.33, 19.67) WHERE employee_id = 2;
@@ -12,18 +12,18 @@ INSERT INTO tips VALUES ("Faisal", "Nasir", "ssk@gmail.com", "some address", 09-
 INSERT INTO user VALUES ('test', 'password');
 
 
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1,1.00, 0.37, 0.63);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2,5.00, 0.52, 4.48);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2,5.00, 0.52, 4.48);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3,2.00, 0.41, 1.59);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1,1.00, 0.37, 0.63);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3,1.00, 0.37, 0.63);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1,1.00, 0.37, 0.63);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2,3.00, 0.44, 2.56);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1,5.00, 0.52, 4.48);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1,2.00, 0.41, 1.59);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3,5.00, 0.52, 4.48);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3,20.00, 1.09, 18.91);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2,10.00, 0.71, 9.29);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1,20.00, 1.09, 18.91);
-INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3,1.00, 0.37, 0.63);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1.00,1.00, 0.37, 0.63);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2.00,5.00, 0.52, 4.48);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2.00,5.00, 0.52, 4.48);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3.00,2.00, 0.41, 1.59);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1.00,1.00, 0.37, 0.63);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3.00,1.00, 0.37, 0.63);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1.00,1.00, 0.37, 0.63);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2.00,3.00, 0.44, 2.56);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1.00,5.00, 0.52, 4.48);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1.00,2.00, 0.41, 1.59);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3.00,5.00, 0.52, 4.48);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3.00,20.00, 1.09, 18.91);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (2.00,10.00, 0.71, 9.29);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (1.00,20.00, 1.09, 18.91);
+INSERT INTO tips (employee_id,tip_amount,tip_fees,tip_final) VALUES (3.00,1.00, 0.37, 0.63);
