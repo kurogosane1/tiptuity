4bbeba259e2d3f5855889c2005d11582a9c04ce2
Syed Khurshid <magnite@hotmail.com>
2021-03-07 21:13:39 -0600

    Got to get the pricing down right

--- front/src/Pages/Front_Page/Front.js
+++ front/src/Pages/Front_Page/Front.js
@@ -7,7 +7,7 @@ import "../../style/FrontPage.css";
 export function Front() {
   return (
     <div className="Main-Page">
-      <div className="main-header">
+      <section className="main-header">
         <Nav />
         <div className="main-content">
           <h1>Tipping Made Easier</h1>
@@ -17,14 +17,54 @@ export function Front() {
           </span>
           <img src={QR} alt="" />
         </div>
-      </div>
-      <div className="main-body">
+      </section>
+      <section className="main-body">
         <img src={Iphone} alt="iphones" />
         <h1>
           Tiptuity has made it easier for you to pay a tip using your phone and
           camera and just pay what you feel
         </h1>
-      </div>
+      </section>
+      <section className="pricing">
+        <div className="pricing_Information">
+          <h1 style={{ fontSize: "3rem" }}>Pricing</h1>
+        </div>
+        <ul className="pricing_List">
+          <li>
+            <div className="pricing_col1">
+              <h1 style={{ fontSize: "3rem", textAlign: "center" }}>Basic</h1>
+              <div>
+                <h1>$225/year</h1>
+              </div>
+              <ul>
+                <li>For Up to 3 Employees Only</li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <div className="pricing_col2">
+              <h1 style={{ fontSize: "3rem", textAlign: "center" }}>Most</h1>
+              <div>
+                <h1>$350/year</h1>
+              </div>
+              <ul>
+                <li>For Up to 5 Employees</li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <div className="pricing_col3">
+              <h1 style={{ fontSize: "3rem", textAlign: "center" }}>Premium</h1>
+              <div>
+                <h1>$500/year</h1>
+              </div>
+              <ul>
+                <li>For Up to 10 Employees</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
