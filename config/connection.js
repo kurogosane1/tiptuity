1540e46d7b3e88f85d3cf8b47f6fb6ede13be007
Syed Khurshid <magnite@hotmail.com>
2021-03-20 18:07:16 -0500

    Got some things working

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,13 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.USERNAME,
-  process.env.PASSWORD,
-  { host: "localhost", dialect: "mysql", logging: false }
+  //This is when working in development environment
+  // process.env.DATABASE_NAME,
+  // process.env.USERNAME,
+  // process.env.PASSWORD,
+  // { host: "localhost", dialect: "mysql", logging: false }
+  process.env.DB_HOST,
+  { dialect: "mysql" }
 );
 
 sequelize.sync({ logging: false });
