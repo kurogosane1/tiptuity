7465d8b78409844233c8813b9af2fd1f412c06c2
Syed Khurshid <magnite@hotmail.com>
2021-03-28 18:09:12 -0500

    some bugs

--- front/src/Layout/Payment/Emp_Switch.js
+++ front/src/Layout/Payment/Emp_Switch.js
@@ -4,17 +4,17 @@ import { AddEmployee } from "../../Pages/AddEmployee";
 import { Front } from "../../Pages/Front_Page/Front";
 import { Login } from "../../Pages/Front_Page/Login/Login";
 import Ind_Emp from "../../Pages/Payment/Ind_Emp";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 import Success from "../../Pages/Payment/Success";
 import Index from "../Dashboard/Index";
 import Protect from "../../Protected/Protect";
 import Failure from "../../Pages/Payment/Failure";
 export function Emp_Switch() {
   //This is for stripe to work with this
-  const stripePromise = loadStripe(
-    process.env.REACT_APP_Stripe_PUBLISHIBLE_KEY
-  );
+  // const stripePromise = loadStripe(
+  //   process.env.REACT_APP_Stripe_PUBLISHIBLE_KEY
+  // );
 
   return (
     <div className="views_payment">
@@ -36,9 +36,7 @@ export function Emp_Switch() {
           <Index />
         </Protect>
         <Route path={"/pay/:id"}>
-          <Elements stripe={stripePromise}>
-            <Ind_Emp />
-          </Elements>
+          <Ind_Emp />
         </Route>
         <Protect path="/AddEmployee">
           <AddEmployee style={{ minHeight: "100vh" }} />
