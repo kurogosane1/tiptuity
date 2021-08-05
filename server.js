f8b2fc738d0e9485c0c5eacd674ac5835a866100
Syed Khurshid <magnite@hotmail.com>
2021-08-05 15:23:43 -0500

    fixed folders for heroku

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Pointing to Router so that Express can use it
-app.use("/api", require("./router/route"));
-app.use("/pay", require("./router/payment"));
+app.use("/api", require("./Router/route"));
+app.use("/pay", require("./Router/payment"));
 
 //This is when to deploy
 if (process.env.NODE_ENV === "production") {
